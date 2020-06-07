c0ab0df6b9b28ef5ae995f6fb972b312ad4f2068
Osamudiamen-Azamegbe <42944794+Osamudiamen-Azamegbe@users.noreply.github.com>
2020-06-07 20:48:35 +0100

    chore: new user model and register form

--- Blog/views.py
+++ Blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from .models import User
+from .forms import UserRegisterForm
 
 
 # Create your views here.
@@ -16,7 +17,16 @@ def index(request):
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your profile @{username} has been created successfully!')
+            return HttpResponseRedirect('/login/')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
 
 
 def register_user(request):
@@ -57,7 +67,7 @@ def register_user(request):
                 new_user = User(first_name=first_name, username=username,
                                 surname=last_name, email=email, password=password)
                 new_user.save()
-                messages.success(request, f'Your profile @{username}\'s has been created successfully!')
+                messages.success(request, f'Your profile @{username} has been created successfully!')
                 return HttpResponseRedirect('/login/')
             else:
                 messages.error(request, f'An account with the username: @{username} '
--- TravelBlog/settings.py
+++ TravelBlog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Blog.apps.BlogConfig',
     'bootstrap4',
+
+
 ]
 
 MIDDLEWARE = [
@@ -114,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
