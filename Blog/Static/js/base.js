f9b815398d8717f7835ee678e02c17d7186dc6ff
osamudiamen <osas.azamegbe@gmail.com>
2020-06-22 22:52:53 +0100

    chore: included navbar in nightmode toggle

--- Blog/Static/js/base.js
+++ Blog/Static/js/base.js
@@ -1,11 +1,16 @@
 const themeSlider = document.querySelector('.slider');
-const navBar = document.nav;
+const navBar = document.querySelector('.navbar-nav');
 themeSlider.addEventListener('click', function(e) {
     // e.preventDefault();
     
     document.body.classList.toggle('dark');
     if(navBar.classList.contains('navbar-light')) {
+        document.nav.classList.toggle('navbar-light');
         navBar.classList.remove('navbar-light');
         navBar.classList.add('navbar-dark');
+    
+    } else if(navBar.classList.contains('navbar-dark')) {
+        navBar.classList.remove('navbar-dark');
+        navBar.classList.add('navbar-light');
     }
 });
