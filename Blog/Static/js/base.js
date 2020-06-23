4ba40104023a37818a90807d3c99dfee5377b7e6
osamudiamen <osas.azamegbe@gmail.com>
2020-06-23 14:00:57 +0100

    chore: added nightmode transition effect for chrome, mozilla, safari, opera

--- Blog/Static/js/base.js
+++ Blog/Static/js/base.js
@@ -1,16 +1,12 @@
 const themeSlider = document.querySelector('.slider');
-const navBar = document.querySelector('.navbar-nav');
 themeSlider.addEventListener('click', function(e) {
     // e.preventDefault();
     
     document.body.classList.toggle('dark');
-    if(navBar.classList.contains('navbar-light')) {
-        document.nav.classList.toggle('navbar-light');
-        navBar.classList.remove('navbar-light');
-        navBar.classList.add('navbar-dark');
+    // if(document.body.classList.contains('dark')) {
+    //     document.body.classList.remove('dark');
     
-    } else if(navBar.classList.contains('navbar-dark')) {
-        navBar.classList.remove('navbar-dark');
-        navBar.classList.add('navbar-light');
-    }
+    // } else {
+    //     document.body.classList.add('dark');
+    // }
 });
