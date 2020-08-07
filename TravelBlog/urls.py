6a661ca561343b7b9107e5f15124269d39ce7b03
osamudiamen <osas.azamegbe@gmail.com>
2020-08-07 20:21:42 +0100

    added media static to urls.py urlpatterns

--- TravelBlog/urls.py
+++ TravelBlog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from Blog import views as blog_views
 
 urlpatterns = [
@@ -27,3 +29,6 @@ urlpatterns = [
     path('map/', include('map.urls')),
     path('accounts/', include('Blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
