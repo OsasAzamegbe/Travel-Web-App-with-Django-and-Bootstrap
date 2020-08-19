c06edecdc69e1220eae04649ac37a649f0c5c345
osamudiamen <osas.azamegbe@gmail.com>
2020-08-19 20:10:26 +0100

    added default png for users

--- Blog/models.py
+++ Blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
     bio = models.TextField(max_length=300)
 
     def __str__(self):
