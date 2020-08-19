880059643efb1da569bc21cfbcfee2d5d7d4419b
osamudiamen <osas.azamegbe@gmail.com>
2020-08-19 15:20:58 +0100

    added unit tests to Blog app. All tests passed

---
+++ Blog/tests/test_models.py
@@ -0,0 +1,3 @@
+from django.test import TestCase
+from Blog.models import Profile
+from Blog.forms import UserRegisterForm
--- map/tests/test_placesAPI.py
+++ map/tests/test_placesAPI.py
@@ -3,4 +3,4 @@ from django.urls import reverse
 
 
 class PlacesAPISearchTest(TestCase):
-    
+    pass
