edea77ea938acbee5a97035ea4243b98415e2e2b
osamudiamen <osas.azamegbe@gmail.com>
2020-08-19 15:28:45 +0100

    started with test for blog mmodels

--- Blog/tests/test_models.py
+++ Blog/tests/test_models.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 from Blog.models import Profile
 from Blog.forms import UserRegisterForm
+
+class TestUser(TestCase):
+
+    def test_register_user(self):
+        pass
+    def test_delete_user(self):
+        pass
+
+
+class TestProfile(TestCase):
+
+    def test_add_profile(self):
+        pass
