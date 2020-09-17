f9a0ae59eaf63fd9500d5739af7935d6ad7a6713
osamudiamen <osas.azamegbe@gmail.com>
2020-09-17 20:09:35 +0100

    chore: added check for post object image and corrected __str__

--- Post/models.py
+++ Post/models.py
@@ -10,20 +10,21 @@ class Post(models.Model):
     content = models.TextField()
     image = models.ImageField(upload_to='post_images', null=True, blank=True)
 
-    def __Str__(self):
+    def __str__(self):
         return f'Author: {self.author}, Title: {self.title}'
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
-        width, height = img.size
-        
-        if width > 1500 and height > 1500:
-            img.resize((1500, 1500), Image.LANCZOS)
+        if self.image:
+            img = Image.open(self.image.path)
+            width, height = img.size
+            
+            if width > 1000 and height > 1000:
+                img.resize((1000, 1000), Image.LANCZOS)
 
-        img.save(self.image.path)
-    
+            img.save(self.image.path)
+        
     class Meta:
         ordering = ['-date']
 
