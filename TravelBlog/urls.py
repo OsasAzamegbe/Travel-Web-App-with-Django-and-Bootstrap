80185f27780446c2db684513fa5de1b4e5b5c8e4
osamudiamen <osas.azamegbe@gmail.com>
2020-09-19 16:07:00 +0100

    reversed media serve in prod

--- TravelBlog/urls.py
+++ TravelBlog/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 from Blog import views as blog_views
 
 urlpatterns = [
@@ -37,5 +36,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += [path('media/<str:path>/', serve, {'document_root': settings.MEDIA_ROOT})]
