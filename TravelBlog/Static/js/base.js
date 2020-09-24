899434f70cf4c56f63161f9e8ce9ce6e16a82ce4
osamudiamen <osas.azamegbe@gmail.com>
2020-09-24 14:13:48 +0100

    extended post favorite toggle to other templates with posts

--- TravelBlog/Static/js/base.js
+++ TravelBlog/Static/js/base.js
@@ -7,7 +7,6 @@ const span = Array.from(favButtons, spanMap);
 let currentFavorites = Array.from(span, currentFavoritesMap);
 let STATE = new Array(favButtons.length).fill(false);
 const url = Array.from(favButtons, urlMap);
-console.log(favButtons, span, currentFavorites, STATE);
 
 
 const likedIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" width="18px" height="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>`
