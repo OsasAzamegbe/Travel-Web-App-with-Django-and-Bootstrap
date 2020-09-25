b29ca34aa5f7d2daf775005dba593ec93254a874
osamudiamen <osas.azamegbe@gmail.com>
2020-09-25 20:28:19 +0100

    debug off

--- TravelBlog/settings.py
+++ TravelBlog/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = SETTINGS_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bini-travel.herokuapp.com', 'bini-travel-staging.herokuapp.com']
 
