6ffded5c701ff9b9fb805c5412b8acac58666b79
osamudiamen <osas.azamegbe@gmail.com>
2020-09-25 20:27:07 +0100

    added post_save signal to create Favorite model for new User

--- Blog/signals.py
+++ Blog/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+from Favorite.models import Favorite
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -11,3 +12,12 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+@receiver(post_save, sender=User)
+def create_favorite(sender, instance, created, **kwargs ):
+    if created:
+        Favorite.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_favorites(sender, instance, **kwargs):
+    instance.favorite.save()
